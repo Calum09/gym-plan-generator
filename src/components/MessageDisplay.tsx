e2cb0bb43cbc8dfe4eb1da93bc94ae167ea91469
Calum Laverick <calumlaverick09@gmail.com>
2023-06-22 18:22:29 +0100

    first commit

---
+++ src/components/MessageDisplay.tsx
@@ -0,0 +1,13 @@
+
+const MessageDisplay = () => {
+    return (
+      <div className="message-display">
+        <p id="icon">X</p>
+        <p>User</p>
+        <p>Message</p>
+      </div>
+    )
+  }
+  
+  export default MessageDisplay
+  
